a51a43b1f0be5bc297ac22b999ad033d01cb4d44
Robert Cole <robhasacamera@gmail.com>
2022-07-22 16:45:51 -0700

    Initial Commit

---
+++ ButtonDemo/ButtonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonDemoApp.swift
+//  ButtonDemo
+//
+//  Created by Robert Cole on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ButtonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonDemo/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ButtonDemo
+//
+//  Created by Robert Cole on 7/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
