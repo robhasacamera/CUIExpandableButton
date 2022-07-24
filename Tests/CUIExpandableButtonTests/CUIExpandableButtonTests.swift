f899001dd30b64f21ed0aec0cf87859b17ba0cb2
Robert Cole <robhasacamera@gmail.com>
2022-07-24 15:44:27 -0700

    Updating test with TODO

--- Tests/CUIExpandableButtonTests/CUIExpandableButtonTests.swift
+++ Tests/CUIExpandableButtonTests/CUIExpandableButtonTests.swift
@@ -3,9 +3,6 @@ import XCTest
 
 final class CUIExpandableButtonTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//        XCTAssertEqual(CUIExpandableButton().text, "Hello, World!")
+        // TODO: Implement some tests.
     }
 }
